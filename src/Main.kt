d29a1f67cad14be41ebea19b2764b8ce374e50ae
Naren <desamnarendrakumar@gmail.com>
2021-02-01 20:58:43 +0530

    Init Commit

---
+++ src/Main.kt
@@ -0,0 +1,34 @@
+var greeting: String? = null
+
+fun main() {
+    // If Cond
+    if (greeting != null) {
+        println(greeting)
+    }
+
+    // When Cond
+    when (greeting) {
+        null -> println("Null Greeting")
+        else -> println(greeting)
+    }
+
+    sayHello()
+    sayHello("Naren")
+    sayHello("Naren", "Welcome")
+}
+
+fun sayHello(){// Return type is Unit
+    println("Hello Unit Function")
+}
+
+fun sayHello(name: String){
+    println("Hello  $name") //  String Templates
+}
+
+
+fun sayHello(name: String, greeting:String){
+    println("$name  $greeting") //  String Templates
+}
+
+// Above all are top-order functions that means they are enclosed in any class,
+// they are free from class level dependency
